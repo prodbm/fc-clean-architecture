2a32f160f8f75f71245a40589819171274b21054
prodbm <ro_bm@hotmail.com>
2024-02-26 22:17:07 -0300

    Validação de Products

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,5 +1,7 @@
+import e from "express";
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
@@ -33,28 +35,10 @@ export default class Product extends Entity implements ProductInterface {
     this.validate();
   }
 
-  validate(): boolean {
-    if (this.id.length === 0) {
-      this.notification.addError({
-        context: "product",
-        message: "Id is required"
-      });
-    }
-    if (this._name.length === 0) {
-      this.notification.addError({
-        context: "product",
-        message: "Name is required"
-      });
-    }
-    if (this._price < 0) {
-      this.notification.addError({
-        context: "product",
-        message: "Price must be greater than zero"
-      });
-    }
+  validate() {
+    ProductValidatorFactory.create().validate(this);
     if (this.notification.hasErrors()) {
       throw new NotificationError(this.notification.getErrors());
     }
-    return true;
   }
 }
